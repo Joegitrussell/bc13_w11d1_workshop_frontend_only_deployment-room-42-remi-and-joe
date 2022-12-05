3fb6316d51b41a5d21c6055125dab3ab8c76c6a4
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2022-12-05 16:10:56 +0000

    batman 2

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ function App() {
         <p>
           Batman
         </p>
-        <img src="https://static.wikia.nocookie.net/marvel_dc/images/4/4b/Batman_Vol_3_86_Textless.jpg/revision/latest?cb=20200502132734Q"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://static.wikia.nocookie.net/marvel_dc/images/4/4b/Batman_Vol_3_86_Textless.jpg/revision/latest?cb=20200502132734Q" alt ="batman"></img>
+     
       </header>
     </div>
   );
